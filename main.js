a3da13de4605b9b29e4f14217a3d9849a63e0404
Apurva Anand <apurvaanand51@gmail.com>
2026-07-02 19:57:21 +0530

    card functionality added

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ const navBtn = document.querySelector(".title h2");
 navBtn.addEventListener("click", () => {
     window.location.reload();
 });
+
+const mandelbrot_01 = document.querySelector("#mandelbrot");
+
+mandelbrot_01.addEventListener("click", () => {
+    window.location.assign("../components/01_mandelbrot/index.html");
+});
